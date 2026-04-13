faee30aca3b46da511b914173685dc5e6ab64df4
desonglll <lindesong666@163.com>
2026-04-13 14:39:13 +0800

    change to comfyui default import method.

--- install.py
+++ install.py
@@ -1,10 +1,2 @@
-# from comfy_env import install
-# install()
-
-# 修改后的 install.py
-def install():
-    print("[Manual Patch] comfy_env.install() skipped. Make sure dependencies are met.")
-    pass
-
-if __name__ == "__main__":
-    install()
+from comfy_env import install
+install()
